8a33c477a31a8baab7a954f2d85c9f22e1066ff7
r-0715 <ritsukiwaritsu@gmail.com>
2026-03-22 17:11:37 +0900

    2-1

--- 2_timer/main.js
+++ 2_timer/main.js
@@ -4,76 +4,93 @@ document.addEventListener('DOMContentLoaded',()=>{
     const set_m = document.getElementById("set_m");
     const set_s = document.getElementById("set_s");
 
-    const displat_h = document.getElementById("display_h");
-    const displat_m = document.getElementById("display_m");
-    const displat_s = document.getElementById("display_s");
+    const display_h = document.getElementById("display_h");
+    const display_m = document.getElementById("display_m");
+    const display_s = document.getElementById("display_s");
+
+    const strat_btn = document.getElementById("start");
+    const stop_btn = document.getElementById("stop");
+
+    const reset_btn = document.getElementById("reset");
 
     set_h.addEventListener('click',()=>{
         //
-        console.log(set_h.value);
-        displat_h.innerHTML = "0"+set_h.value;
-    });
-});
+        let hours = Number(set_h.value);
 
-/* 
-<option value="0">0</option>
-<option value="1">1</option>
-<option value="2">2</option>
-<option value="3">3</option>
-<option value="4">4</option>
-<option value="5">5</option>
-<option value="6">6</option>
-<option value="7">7</option>
-<option value="8">8</option>
-<option value="9">9</option>
-<option value="10">11</option>
-<option value="11">12</option>
-<option value="12">13</option>
-<option value="13">14</option>
-<option value="14">15</option>
-<option value="15">16</option>
-<option value="16">17</option>
-<option value="17">18</option>
-<option value="18">18</option>
-<option value="19">19</option>
-<option value="20">20</option>
-<option value="21">21</option>
-<option value="22">22</option>
-<option value="23">23</option>
-<option value="24">24</option>
-<option value="25">25</option>
-<option value="26">26</option>
-<option value="27">27</option>
-<option value="28">28</option>
-<option value="29">29</option>
-<option value="30">30</option>
-<option value="31">31</option>
-<option value="32">32</option>
-<option value="33">33</option>
-<option value="34">34</option>
-<option value="35">35</option>
-<option value="36">36</option>
-<option value="37">37</option>
-<option value="38">38</option>
-<option value="39">39</option>
-<option value="40">40</option>
-<option value="41">41</option>
-<option value="42">42</option>
-<option value="43">43</option>
-<option value="44">44</option>
-<option value="45">45</option>
-<option value="46">46</option>
-<option value="47">47</option>
-<option value="48">48</option>
-<option value="49">49</option>
-<option value="50">50</option>
-<option value="51">51</option>
-<option value="52">52</option>
-<option value="53">53</option>
-<option value="54">54</option>
-<option value="55">55</option>
-<option value="56">56</option>
-<option value="57">57</option>
-<option value="58">58</option>
-<option value="59">59</option> 
-*/
+        if(hours < 10) display_h.innerHTML = "0"+hours; else display_h.innerHTML = hours;
+
+        //set_h.style.pointerEvents = "none";
+    });
+
+    set_m.addEventListener('click',()=>{
+        //
+        let minutes = Number(set_m.value);
+
+        if(minutes < 10) display_m.innerHTML = "0"+minutes; else display_m.innerHTML = minutes;
+    });
+
+    set_s.addEventListener('click',()=>{
+        //
+        let second = Number(set_s.value);
+
+        if(second < 10) display_s.innerHTML = "0"+second; else display_s.innerHTML = second;
+    });
+
+    let id;
+    let time;
+    
+    strat_btn.addEventListener('click',()=>{
+        //
+        strat_btn.disabled = true;
+        stop_btn.disabled = false;
+
+        if(strat_btn.value == "first"){
+            let hours = Number(set_h.value);
+            let minutes = Number(set_m.value);
+            let second = Number(set_s.value);
+
+            time = hours*3600 + minutes*60 + second;
+
+            strat_btn.value = "other";
+        }
+        //console.log(time);
+        id = setInterval(() => {
+            time -= 1;
+            
+            let hours = Math.floor(time/3600);
+            let minutes = Math.floor((time-hours*3600)/60);
+            let second = time - (hours*3600 + minutes*60);
+
+            if(hours < 10) display_h.innerHTML = "0"+hours; else display_h.innerHTML = hours;
+            if(minutes < 10) display_m.innerHTML = "0"+minutes; else display_m.innerHTML = minutes;
+            if(second < 10) display_s.innerHTML = "0"+second; else display_s.innerHTML = second;
+        }, 1000);
+    });
+
+    stop_btn.addEventListener('click',()=>{
+        //
+        stop_btn.disabled = true;
+        strat_btn.disabled = false;
+
+        clearInterval(id);
+    });
+
+    reset_btn.addEventListener('click',()=>{
+        //
+        display_h.innerHTML = "00";
+        display_m.innerHTML = "00";
+        display_s.innerHTML = "00";
+
+        set_h.value = "0";
+        set_m.value = "0";
+        set_s.value = "0";
+
+        strat_btn.value = "first";
+
+        clearInterval(id);
+
+        strat_btn.disabled = false;
+        stop_btn.disabled = true;
+    });
+    //
+});
